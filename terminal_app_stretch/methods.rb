40e41c4dbce61f003427add58f6ca06b95681fad
chelseal <chelseablove@gmail.com>
2019-09-04 15:46:48 +1000

    Building hash for feature 2

--- terminal_app_stretch/methods.rb
+++ terminal_app_stretch/methods.rb
@@ -1,4 +1,17 @@
 # Feature 2: Library of stretches
 # Seek input for: Legs, Back, Core, Shoulders, Chest, Neck, Arms, Wrists/Forearm
 
+body = {
+    lower_body: {
+        legs: "Hamstring stretch",
+},
+    core_chest: {
+        chest: "Shoulder stretch",
+        core: "Torso stretch",
+        back: "Upper back stretch",
+    }
+}
+
+p body[:core_chest][:chest]
+
 
