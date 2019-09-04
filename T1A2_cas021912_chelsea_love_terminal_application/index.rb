dd609254898fcdef016b88b66ca594500c8e7746
chelseal <chelseablove@gmail.com>
2019-09-04 14:42:44 +1000

    Renamed files

---
+++ T1A2_cas021912_chelsea_love_terminal_application/index.rb
@@ -0,0 +1,27 @@
+# Feature 1: Personalisation
+# Seek input for: name, length of work sprint, desk setup (consideration)
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "Thanks #{name}, how long was your working sprint in minutes?"
+time = gets.chomp
+
+# puts "Thanks #{name}, what is your desk setup?"
+# puts "1 = desk_type_1"
+# puts "2 = desk_type_2"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
