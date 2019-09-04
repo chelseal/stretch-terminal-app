97b7b55eefd729eecf41208f38d8dfb0b10761ca
chelseal <chelseablove@gmail.com>
2019-09-04 16:04:27 +1000

    Built-up hash on feature 2

--- terminal_app_stretch/methods.rb
+++ terminal_app_stretch/methods.rb
@@ -4,14 +4,19 @@
 body = {
     lower_body: {
         legs: "Hamstring stretch",
-},
+    },
     core_chest: {
         chest: "Shoulder stretch",
         core: "Torso stretch",
         back: "Upper back stretch",
     }
+    upper_body: {
+        shoulder: "Upper trap stretch",
+        wrist_forearm: "Wrist and forearm stretch",
+        arms: "Upper body and arm stretch",
+        neck: "Neck stretch"
+    }
 }
 
-p body[:core_chest][:chest]
-
+p body[:upper_body][:arms]
 
