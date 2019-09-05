cb2b3d2ae4a6b47f0a6f6d0b0a773268290bc23b
chelseal <chelseablove@gmail.com>
2019-09-05 14:05:36 +1000

    Feature 2 variables updated

--- terminal_app_stretch/methods_def.rb
+++ terminal_app_stretch/methods_def.rb
@@ -1,21 +1,20 @@
-Upper_trap_stretch = "Gently pull your head toward each shoulder until a light stretch is felt. Hold the pose for 10 to 15 seconds. Alternate once on each side."
-
-Pectoralis_stretch = "Clasp hands behind your back.
-Push the chest outward, and raise the chin.
-Hold the pose for 10 to 30 seconds."
-
-Chest = "Shoulder, or pectoralis stretch. Clasp hands behind your back. Push the chest outward, and raise the chin. Hold the pose for 10 to 30 seconds."
-
-Upper_back_stretch = "Clasp your hands in front of you and lower your head in line with your arms. Press forward and hold for 10 to 30 seconds."
-
-Back_stretch = "Forward stretch, rhomboid upper or upper back stretch. Clasp your hands in front of you and lower your head in line with your arms. Press forward and hold for 10 to 30 seconds.""
-
-Torso_stretch = "Keep your feet firmly on the ground, facing forward. Twist your upper body in the direction of the arm that’s resting on the back of your chair. Hold pose for 10 to 30 seconds. Repeat on other side. Tip: Exhale as you lean into the stretch for a greater range of motion."
-
-Hamstrings_stretch = "Remaining seated, extend one leg outward. Reach toward your toes. Hold for 10 to 30 seconds. Repeat on the other side. Be sure to do this one leg at a time, as doing this exercise with both legs out can cause back issues."
-
+# Upper body stretches grouping
 Shoulders_stretch = "Raise both shoulders at once up toward the ears. Drop them and repeat 10 times each direction."
+
+Wrist_forearm_stretch = "Straighten your arm, holding your fingers, gently bend wrist up until you feel a stretch in the forearm. Hold for 15 seconds then release. Holding your fingers, gently bend wrist down until you feel a stretch in the forearm, hold for 15 seconds then release. Repeat with your other arm."
+
+Upper_trap_stretch = "Gently pull your head toward each shoulder until a light stretch is felt. Hold the pose for 10 to 15 seconds. Alternate once on each side."
 
 Neck_stretch = "Relax and lean your head forward. Slowly roll toward one side and hold for 10 seconds. Repeat on other side. Relax again and lift your chin back to starting position. Do this three times for each direction."
 
-Wrist_forearm_stretch = "Straighten your arm, holding your fingers, gently bend wrist up until you feel a stretch in the forearm. Hold for 15 seconds then release. Holding your fingers, gently bend wrist down until you feel a stretch in the forearm, hold for 15 seconds then release. Repeat with your other arm."
+# Chest and core stretches grouping
+
+Pectoralis_stretch = "Clasp hands behind your back. Push the chest outward, and raise the chin. Hold the pose for 10 to 30 seconds."
+
+Torso_stretch = "Keep your feet firmly on the ground, facing forward. Twist your upper body in the direction of the arm that’s resting on the back of your chair. Hold pose for 10 to 30 seconds. Repeat on other side. Tip: Exhale as you lean into the stretch for a greater range of motion."
+
+Upper_back_stretch = "Clasp your hands in front of you and lower your head in line with your arms. Press forward and hold for 10 to 30 seconds."
+
+# Lower body stretches grouping
+
+Hamstrings_stretch = "Remaining seated, extend one leg outward. Reach toward your toes. Hold for 10 to 30 seconds. Repeat on the other side. Be sure to do this one leg at a time, as doing this exercise with both legs out can cause back issues."
