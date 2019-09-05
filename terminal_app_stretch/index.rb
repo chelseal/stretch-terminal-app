92c4bc157aaba42f939af1f41615c4e60eed22aa
chelseal <chelseablove@gmail.com>
2019-09-05 13:36:37 +1000

    Completed stretch feature 2 variable assignment and updated case statement

--- terminal_app_stretch/index.rb
+++ terminal_app_stretch/index.rb
@@ -1,3 +1,5 @@
+require_relative "./methods_def.rb"
+
 # Feature 1: Personalisation
 # Seek input for: name, length of work sprint
 
@@ -26,17 +28,47 @@ body = {
 
 case time
 when 0..15
-    puts "Would you like to stretch your:\n\ Shoulders, Wrists or forearms, Arms, Neck"
-    input = gets.chomp
+    puts "Would you like to stretch your:\n\ 1) Shoulders, (2) Wrists or forearms, (3) Arms, (4) Neck \n\ Please enter the number"
+
+input = gets.chomp
+
+    if input.to_i == 1
+        puts "Torso stretch"
+    end
+
+    if input.to_i == 2
+        puts "Pectoralis stretch"
+    end
+
+    if input.to_i == 3
+        puts Upper_back_stretch
+    end
+
+
 when 16..25
-    puts "Would you like to stretch your:\n\ Core, Chest or Back"
-    input = gets.chomp
-    Core = true
-    Chest = true
-    Back = true
+    puts "Would you like to stretch your:\n\ (1) Core, (2) Chest or (3) Back \n\ Please enter the number"
+
+input = gets.chomp
+
+    if input.to_i == 1
+        puts "Torso stretch"
+    end
+
+    if input.to_i == 2
+        puts "Pectoralis stretch"
+    end
+
+    if input.to_i == 3
+        puts Upper_back_stretch
+
+    end
+
 when 26..240
-    puts "Would you like to stretch your:\n\ Legs"
-    input = gets.chomp
+    puts "Would you like to stretch your:\n\ (1) Legs \n\ Please enter the number"
+
+input = gets.chomp
+
+
 else
     puts "Please re-enter the length of your working sprint in minutes. For example: 11 minutes working sprint = simply type '11'"
 end
--- terminal_app_stretch/methods_def.rb
+++ terminal_app_stretch/methods_def.rb
@@ -1,69 +1,21 @@
-## Arms
-# Upper trap stretch
-# Gently pull your head toward each shoulder until a light stretch is felt.
-# Hold the pose for 10 to 15 seconds.
-# Alternate once on each side.
+Upper_trap_stretch = "Gently pull your head toward each shoulder until a light stretch is felt. Hold the pose for 10 to 15 seconds. Alternate once on each side."
 
 Pectoralis_stretch = "Clasp hands behind your back.
 Push the chest outward, and raise the chin.
 Hold the pose for 10 to 30 seconds."
 
-## Chest
-# Shoulder, or pectoralis stretch
-# Clasp hands behind your back.
-# Push the chest outward, and raise the chin.
-# Hold the pose for 10 to 30 seconds.
+Chest = "Shoulder, or pectoralis stretch. Clasp hands behind your back. Push the chest outward, and raise the chin. Hold the pose for 10 to 30 seconds."
 
-Upper_back_stretch = "Clasp your hands in front of you and lower your head in line with your arms.
-Press forward and hold for 10 to 30 seconds."
+Upper_back_stretch = "Clasp your hands in front of you and lower your head in line with your arms. Press forward and hold for 10 to 30 seconds."
 
-## Back
-# Forward stretch, rhomboid upper or upper back stretch
-# Clasp your hands in front of you and lower your head in line with your arms.
-# Press forward and hold for 10 to 30 seconds.
+Back_stretch = "Forward stretch, rhomboid upper or upper back stretch. Clasp your hands in front of you and lower your head in line with your arms. Press forward and hold for 10 to 30 seconds.""
 
-Torso_stretch = "Keep your feet firmly on the ground, facing forward.
-Twist your upper body in the direction of the arm that’s resting on the back of your chair.
-Hold pose for 10 to 30 seconds.
-Repeat on other side.
-Tip: Exhale as you lean into the stretch for a greater range of motion.
-"
+Torso_stretch = "Keep your feet firmly on the ground, facing forward. Twist your upper body in the direction of the arm that’s resting on the back of your chair. Hold pose for 10 to 30 seconds. Repeat on other side. Tip: Exhale as you lean into the stretch for a greater range of motion."
 
-## Core
-# Torso stretch, or trunk rotation
-# Keep your feet firmly on the ground, facing forward.
-# Twist your upper body in the direction of the arm that’s resting on the back of your chair.
-# Hold pose for 10 to 30 seconds.
-# Repeat on other side.
-# Tip: Exhale as you lean into the stretch for a greater range of motion.
+Hamstrings_stretch = "Remaining seated, extend one leg outward. Reach toward your toes. Hold for 10 to 30 seconds. Repeat on the other side. Be sure to do this one leg at a time, as doing this exercise with both legs out can cause back issues."
 
-# ## Legs
-# # Hamstrings stretch
-# Remaining seated, extend one leg outward.
-# Reach toward your toes.
-# Hold for 10 to 30 seconds.
-# Repeat on the other side.
-# Be sure to do this one leg at a time, as doing this exercise with both legs out can cause back issues.
+Shoulders_stretch = "Raise both shoulders at once up toward the ears. Drop them and repeat 10 times each direction."
 
-# ## Shoulders
-# # Shoulder shrug
-# Raise both shoulders at once up toward the ears.
-# Drop them and repeat 10 times each direction.
+Neck_stretch = "Relax and lean your head forward. Slowly roll toward one side and hold for 10 seconds. Repeat on other side. Relax again and lift your chin back to starting position. Do this three times for each direction."
 
-# ## Neck
-# # Neck stretches
-# Relax and lean your head forward.
-# Slowly roll toward one side and hold for 10 seconds.
-# Repeat on other side.
-# Relax again and lift your chin back to starting position.
-# Do this three times for each direction.
-
-## Wrist/forearm
-# Wrist and forearm exercises
-# SafeWork NSW
-# straighten your arm
-# holding your fingers, gently bend wrist up until you feel a stretch in the forearm
-# hold for 15 seconds then release
-# holding your fingers, gently bend wrist down until you feel a stretch in the forearm
-# hold for 15 seconds then release
-# repeat with your other arm
+Wrist_forearm_stretch = "Straighten your arm, holding your fingers, gently bend wrist up until you feel a stretch in the forearm. Hold for 15 seconds then release. Holding your fingers, gently bend wrist down until you feel a stretch in the forearm, hold for 15 seconds then release. Repeat with your other arm."
